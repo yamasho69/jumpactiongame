5b73eb5c900490e539da0af7a46a04b61b4c69ea
shoheiyamamoto <nitiham55@yahoo.co.jp>
2018-10-02 19:28:02 +0900

    9回目のコミット(音が出ない)

--- core/src/jp/techacademy/shohei/yamamoto/jumpactiongame/Enemy.java
+++ core/src/jp/techacademy/shohei/yamamoto/jumpactiongame/Enemy.java
@@ -1,45 +1,16 @@
 package jp.techacademy.shohei.yamamoto.jumpactiongame;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class Enemy extends GameObject {
-    // 横幅、高さ
+public class Enemy extends Sprite {
+    //横幅、高さ
     public static final float ENEMY_WIDTH = 0.8f;
     public static final float ENEMY_HEIGHT = 0.8f;
 
-    // タイプ（通常と動くタイプ）
-    public static final int ENEMY_TYPE_STATIC = 0;
-    public static final int ENEMY_TYPE_MOVING = 1;
-    
-    // 速度
-    public static final float ENEMY_VELOCITY = 1.0f;
-
-    int mType;
-    int mState;
-
-    public Enemy(int type, Texture texture, int srcX, int srcY, int srcWidth, int srcHeight) {
+    public Enemy(Texture texture, int srcX, int srcY, int srcWidth, int srcHeight) {
         super(texture, srcX, srcY, srcWidth, srcHeight);
         setSize(ENEMY_WIDTH, ENEMY_HEIGHT);
-        mType = type;
-        if (mType == ENEMY_TYPE_MOVING) {
-            velocity.x = ENEMY_VELOCITY;
-        }
-    }
-
-    // 座標を更新する
-    public void update(float deltaTime) {
-        if (mType == ENEMY_TYPE_MOVING) {
-            setX(getX() + velocity.x * deltaTime);
-
-            if (getX() < ENEMY_WIDTH / 2) {
-                velocity.x = -velocity.x;
-                setX(ENEMY_WIDTH / 2);
-            }
-            if (getX() > GameScreen.WORLD_WIDTH - ENEMY_WIDTH / 2) {
-                velocity.x = -velocity.x;
-                setX(GameScreen.WORLD_WIDTH - ENEMY_WIDTH / 2);
-            }
-        }
     }
 }
 
